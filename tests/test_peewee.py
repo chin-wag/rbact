c0cec054ee844e3eb83132e6e4fc219346f9958a
chin-wag <svetlanafedorishcheva99@gmail.com>
2021-11-29 19:32:03 +0500

    refactor: pre-commit

--- tests/test_peewee.py
+++ tests/test_peewee.py
@@ -5,37 +5,37 @@ from rbact import Inspector
 
 
 def test_inspector_with_peewee_adapter():
-    db = connect('sqlite:///:memory:')
+    db = connect("sqlite:///:memory:")
 
     adapter = rbact_peewee.PeeweeAdapter(db)
     adapter.create_tables()
 
-    rbact_peewee.Users.create(login='user1', id=1)
-    rbact_peewee.Roles.create(name='role1', id=1)
+    rbact_peewee.Users.create(login="user1", id=1)
+    rbact_peewee.Roles.create(name="role1", id=1)
     rbact_peewee.UsersRoles.create(user_id=1, role_id=1)
-    rbact_peewee.Rules.create(role_id=1, obj='resource1', act='read')
+    rbact_peewee.Rules.create(role_id=1, obj="resource1", act="read")
 
     inspector = Inspector(adapter)
-    res = inspector.has_access('user1', 'resource1', 'read')
+    res = inspector.has_access("user1", "resource1", "read")
     assert res
-    res = inspector.has_access('user1', 'resource1', 'write')
+    res = inspector.has_access("user1", "resource1", "write")
     assert not res
-    res = inspector.has_access('user1', 'resource2', 'write')
+    res = inspector.has_access("user1", "resource2", "write")
     assert not res
-    res = inspector.has_access('user2', 'resource2', 'write')
+    res = inspector.has_access("user2", "resource2", "write")
     assert not res
 
-    rbact_peewee.Roles.create(name='role2', id=2)
+    rbact_peewee.Roles.create(name="role2", id=2)
     rbact_peewee.UsersRoles.create(user_id=1, role_id=2)
-    rbact_peewee.Rules.create(role_id=2, obj='resource1', act='write')
+    rbact_peewee.Rules.create(role_id=2, obj="resource1", act="write")
 
-    res = inspector.has_access('user1', 'resource1', 'write')
+    res = inspector.has_access("user1", "resource1", "write")
     assert res
 
-    rbact_peewee.Users.create(login='user2', id=2)
-    rbact_peewee.Roles.create(name='role3', id=3, parent=2)
+    rbact_peewee.Users.create(login="user2", id=2)
+    rbact_peewee.Roles.create(name="role3", id=3, parent=2)
     rbact_peewee.UsersRoles.create(user_id=2, role_id=3)
-    res = inspector.has_access('user2', 'resource1', 'write')
+    res = inspector.has_access("user2", "resource1", "write")
     assert res
-    res = inspector.has_access('user2', 'resource1', 'read')
+    res = inspector.has_access("user2", "resource1", "read")
     assert not res
